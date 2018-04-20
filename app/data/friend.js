b45457ab6269a9ac9d1d9f9f934143def2a1e5ce
Eitan Fine <33404825+EitanFine@users.noreply.github.com>
2018-04-20 14:53:42 -0400

    Homework --Friend Finder

---
+++ app/data/friend.js
@@ -0,0 +1,58 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+  {
+    name: 'Beyonce',
+    profilePic: 'http://cdn2.stylecraze.com/wp-content/uploads/2013/07/Pictures-Of-Beyonce-Without-Makeup.jpg',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+var friendList = [
+  {
+    name: 'Justin Bieber',
+    profilePic: 'https://images-na.ssl-images-amazon.com/images/I/61IgxSXUr+L.png',
+    scores: [
+      "4",
+      "3",
+      "2",
+      "2",
+      "4",
+      "5",
+      "5",
+      "4",
+      "3",
+      "3"
+    ]
+  },
+  {
+    name: 'Kendall Jenner',
+    profilePic: 'https://media.wmagazine.com/photos/5a5f5f33c4341d3bec531e85/4:3/w_1536/kendall-jenner-blake-griffin-never-serious-web-lead.jpg',
+    scores: [
+      "2",
+      "3",
+      "5",
+      "4",
+      "5",
+      "3",
+      "4",
+      "4",
+      "5",
+      "5"
+    ]
+  }
+  
+]
+
+module.exports = friendList;
